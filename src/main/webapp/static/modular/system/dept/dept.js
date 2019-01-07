0bc09d2f8d3e5eef5602749c29ad1d9613040795
david_wang <david_wang@comwave.com.cn>
2019-01-07 17:15:30 +0800

    部门管理增加考勤相关字段

--- src/main/webapp/static/modular/system/dept/dept.js
+++ src/main/webapp/static/modular/system/dept/dept.js
@@ -16,9 +16,23 @@ Dept.initColumn = function () {
         {field: 'selectItem', radio: true},
         {title: 'id', field: 'id', align: 'center', valign: 'middle',width:'50px'},
         {title: '部门简称', field: 'simplename', align: 'center', valign: 'middle', sortable: true},
-        {title: '部门全称', field: 'fullname', align: 'center', valign: 'middle', sortable: true},
+        // {title: '部门全称', field: 'fullname', align: 'center', valign: 'middle', sortable: true},
         {title: '排序', field: 'num', align: 'center', valign: 'middle', sortable: true},
-        {title: '备注', field: 'tips', align: 'center', valign: 'middle', sortable: true}];
+        //{title: '备注', field: 'tips', align: 'center', valign: 'middle', sortable: true},
+        {title: '打卡次数', field: 'attend_times', visible: true, align: 'center', valign: 'middle'},
+        {title: '上班时间', field: 'start_work_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '下班时间', field: 'end_work_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '午休开始', field: 'start_rest_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '午休结束', field: 'end_rest_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '离岗时间', field: 'leave_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '加班时间', field: 'start_over_time', visible: true, align: 'center', valign: 'middle'},
+        {title: '邮箱1', field: 'email1', visible: true, align: 'center', valign: 'middle'},
+        {title: '邮箱2', field: 'email2', visible: true, align: 'center', valign: 'middle'},
+        {title: '邮箱3', field: 'email3', visible: true, align: 'center', valign: 'middle'},
+        {title: '发送周期', field: 'send_email_cycle', visible: true, align: 'center', valign: 'middle'},
+        {title: '工作日', field: 'work_day', visible: true, align: 'center', valign: 'middle'}
+
+        ];
 };
 
 /**
@@ -42,7 +56,7 @@ Dept.openAddDept = function () {
     var index = layer.open({
         type: 2,
         title: '添加部门',
-        area: ['800px', '420px'], //宽高
+        area: ['800px', '800px'], //宽高
         fix: false, //不固定
         maxmin: true,
         content: Feng.ctxPath + '/dept/dept_add'
@@ -58,7 +72,7 @@ Dept.openDeptDetail = function () {
         var index = layer.open({
             type: 2,
             title: '部门详情',
-            area: ['800px', '420px'], //宽高
+            area: ['800px', '800px'], //宽高
             fix: false, //不固定
             maxmin: true,
             content: Feng.ctxPath + '/dept/dept_update/' + Dept.seItem.id
